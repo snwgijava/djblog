a1fa0a417c1bfc936dc0c8c13c3607c963e21a74
yangjian <809127232@qq.com>
2018-06-26 17:28:39 +0800

    修复部份样式显示问题

--- static/js/blog.js
+++ static/js/blog.js
@@ -6,20 +6,23 @@ function refresh_captcha(event) {
     });
     return false;
 }
+
 // 登陆和注册验证码
 $('#login_form .captcha').click({'form_id': 'login_form'}, refresh_captcha);
 $('#register_form .captcha').click({'form_id': 'register_form'}, refresh_captcha);
 
 
- $("#up").hide();         //刚进入页面时设置为隐藏
-    $("#up").bind("click", function () {     //单击时返回顶部
-        $('html, body').animate({scrollTop: 0}, 300);
-        return false;
-    });
-    $(window).bind('scroll resize', function () {   //判断页面所在的位置，小于300就隐藏，否则就显示
-        if ($(window).scrollTop() <= 300) {
-            $("#up").hide();
-        } else {
-            $("#up").show();
-        }
-    });
+$('#up-top').hide();         //刚进入页面时设置为隐藏
+$("#up-top").bind("click", function () {     //单击时返回顶部
+    $('html, body').animate({scrollTop: 0}, 300);
+    return false;
+});
+$(window).bind('scroll resize', function () {   //判断页面所在的位置，小于300就隐藏，否则就显示
+    if ($(window).scrollTop() <= 300) {
+        $("#up-top").hide();
+    } else {
+        $("#up-top").show();
+    }
+});
+
+
